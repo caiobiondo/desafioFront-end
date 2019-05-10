d2829b1409bc5569b0f1c3075bff324fa53d70a0
Caio <caio.a.biondo@gmail.com>
2019-05-10 12:29:01 -0300

    Testes

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import React, {Component} from 'react';
+
+export default class Imput extends Component {
+  render () {
+    return(
+      <div>
+        <label>some props</label>
+       
+        </div>
+    );
+  }
+}
+
+
+
+
